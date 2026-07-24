b84999fdbe4d7d1e86e4e83649e678646b294862
HCyrano <hcyrano@free.fr>
2026-07-24 09:45:59 +0200

    dev-2026-07-24 09h45 origin

--- Roxane/main.cpp
+++ Roxane/main.cpp
@@ -97,7 +97,7 @@ int main (int argc, char * argv[]) {
 		}
 	}
 
-    std::string version = "build-2026-07-24 09h30";
+    std::string version = "dev-2026-07-24 09h45";
     std::string vers_eval = RXEvaluation::get_version();
 
 	std::cout << "Version Melody " << version << std::endl;
